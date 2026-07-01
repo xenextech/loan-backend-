89fbdac4740cc9ee265003ae6b50def21aad052a
Rabi Gautam <rabigautam43@gmail.com>
2026-07-01 09:21:10 +0545

    fix:credit score

--- src/modules/creditScore/credit-score.service.ts
+++ src/modules/creditScore/credit-score.service.ts
@@ -350,7 +350,7 @@ export class CreditScoreService {
     if (percentage <= MEDIUM_RISK_THRESHOLD) return CreditGrade.A3;
     if (percentage <= MEDIUM_HIGH_RISK_THRESHOLD) return CreditGrade.A4;
 
-    return CreditGrade.NA;
+    return CreditGrade.B;
   }
   private getRiskRating(percentage: number): {
     riskCategory: string;
