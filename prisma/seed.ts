037fc2f57e503ada0f9e204d58ed1575cd20e1bc
Rabi Gautam <rabigautam43@gmail.com>
2026-07-01 13:32:55 +0545

    feat:seed added for prisma setup

---
+++ prisma/seed.ts
@@ -0,0 +1,56 @@
+import { PrismaClient, UserRole } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
+import { PrismaPg } from '@prisma/adapter-pg';
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL ?? '',
+});
+const prisma = new PrismaClient({ adapter });
+
+async function main() {
+  const password = 'Password@123';
+  const passwordHash = await bcrypt.hash(password, 12);
+
+  const users = [
+    {
+      email: 'initiator@yopmail.com',
+      role: UserRole.INITIATOR,
+    },
+    {
+      email: 'approver@yopmail.com',
+      role: UserRole.APPROVER,
+    },
+    {
+      email: 'supporter@yopmail.com',
+      role: UserRole.SUPPORTER,
+    },
+  ];
+
+  for (const user of users) {
+    await prisma.user.upsert({
+      where: {
+        email: user.email,
+      },
+      update: {
+        role: user.role,
+      },
+      create: {
+        email: user.email,
+        passwordHash,
+        role: user.role,
+        isEmailVerified: true,
+      },
+    });
+  }
+
+  console.log(' Seed completed successfully.');
+  console.log('Default Password:', password);
+}
+
+main()
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
